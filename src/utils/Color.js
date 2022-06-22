8850c8066f68382fc0fdb696065ca0034ca7576d
sonmez27 <snmezmustafa@hotmail.com>
2022-06-22 15:45:42 +0300

    ThemeMode

--- src/utils/Color.js
+++ src/utils/Color.js
@@ -1,7 +1,7 @@
 import {Appearance} from 'react-native';
 
-//const colorScheme = Appearance.getColorScheme();
-const colorScheme = 'dark';
+const colorScheme = Appearance.getColorScheme();
+//const colorScheme = 'dark';
 const Background = colorScheme === 'dark' ? '#1D3557' : '#F1FAEE';
 const FontColor = colorScheme === 'dark' ? '#F1FAEE' : '#1D3557';
 const Container = colorScheme === 'dark' ? '#457B9D' : '#A8DADC';
